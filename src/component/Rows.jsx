8cbccf3abffcebeff3aed4b25b5836df9ddc137b
daniel.emeka@blinqpay.io <daniel.emeka@blinqpay.io>
2022-04-07 15:29:56 +0100

    latest

--- src/component/Rows.jsx
+++ src/component/Rows.jsx
@@ -1,33 +1,60 @@
 import React,{useState,useEffect}from 'react'
 import instance from '../axios/axios';
-import requests from '../axios/request';
-import './Rows.css'
+import './Rows.css';
+import YouTube from 'react-youtube';
+import movieTrailer from "movie-trailer";
+
 
 const base_url = "http://image.tmdb.org/t/p/original/"
 function Rows({title , fetchurl , isLargeRow}) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl , setTrailerUrl] = useState("")
 
   useEffect(() => {
     async function fetchData(){
        const request =  await instance.get(fetchurl);
        console.log(request.data.results)
        setMovies(request.data.results)
+       return request
     }
     fetchData()
   }, [fetchurl])
-  
+
+
+  const opts = {
+    height: '390',
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    }
+  }
+
+const handleClick = (movie) =>{
+if(trailerUrl){
+  setTrailerUrl("");
+}else {
+  movieTrailer(movie?.name || "").then((url) => {
+    const urlParams = new URLSearchParams(new URL(url).search);
+    setTrailerUrl( urlParams.get('v'));
+  }).catch((error)=> console.log(error))
+}
+}
+
+
   return (
     <div className='row'>
       <h1>{title}</h1>
 <div className="row__posters">
 
         {movies.map((movie)=>(
-           <img className={`row_poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+           <img onClick={()=> handleClick(movie)} className={`row_poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
         ))}
       
 </div>
-      
+            { trailerUrl &&  <YouTube videoId={trailerUrl} opts={opts}/>}
+           
     </div>
+    
   )
 }
 
